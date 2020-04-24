14783421f6094c1f74e961819d3a430b04e64604
isakchoe <50509176+isakchoe@users.noreply.github.com>
2020-04-24 22:52:25 +0900

    complete all functions!!

--- manage.py
+++ manage.py
@@ -8,31 +8,28 @@ client = MongoClient('localhost', 27017)
 db = client.dbhi
 
 
-
-
-
 ## HTML을 주는 부분
 @app.route('/')
 def home():
     return render_template('home.html')
 
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
-
 ## API 역할을 하는 부분
 @app.route('/api/post', methods=['POST'])
-def test_post():
-    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+def di_post():
+    first = int(request.form['first'])
+    last = int(request.form['last'])
+    result = list(db.dinings.find({'price': {'$gt': first, '$lt': last}}, {'_id': False}))
+
+    print(result)
+
+    return jsonify({'result': 'success', 'result': result})
 
 
 @app.route('/api/list', methods=['GET'])
 def di_get():
-    # 1. mystar 목록 전체를 검색합니다. ID는 제외하고 like 가 많은 순으로 정렬합니다.
+
     dinings = list(db.dinings.find({}, {'_id': False}))
-    # 2. 성공하면 success 메시지와 함께 stars 목록을 클라이언트에 전달합니다.
     return jsonify({'result': 'success', 'di_list': dinings})
 
 
